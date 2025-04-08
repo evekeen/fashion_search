ee4e63f60424ff2e865c70db30863a153eaa44a1
Alexander Ivkin <a@ivkin.dev>
2025-04-07 21:03:24 -0400

    added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/next';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,9 +13,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
-      <SpeedInsights />
       <Component {...pageProps} />
-    </>
+      <SpeedInsights />
+      <Analytics />
   );
 }
 
